db73863404cdcb134f4707bcfe5932c7acdfe4e4
aladinus0 <aladinxaws@gmail.com>
2026-03-23 18:17:25 +0800

    update script

--- ascii-script.sh
+++ ascii-script.sh
@@ -3,5 +3,5 @@
 echo "Executing from script"
 ls -ltr
 sudo apt-get install cowsay -y
-cowsay -f dragon "Awwwww Awwwww" > dragon.txt
+cowsay -f dragon "Awwwww Awwwww Im a dragon" > dragon.txt
 cat dragon.txt
